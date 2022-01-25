b2cd34c31015198fe57b1eb3b4a9ffeced66cc7b
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-25 22:36:28 +0530

    custom store

--- src/Cart/cart-store.js
+++ src/Cart/cart-store.js
@@ -13,6 +13,26 @@ let items = [
   }
 ];
 
-const cart = writable(items);
+function createCart() {
+  const cart = writable(items);
 
-export default cart;
+  return {
+    subscribe: cart.subscribe,
+    addItem: (itemToBeAdded) => {
+      cart.update((itemsOfCart) => {
+        if (itemsOfCart.find(i => i.id === itemToBeAdded.id)) {
+          return [...itemsOfCart];
+        }
+        return [...itemsOfCart, itemToBeAdded];
+      });
+    },
+    removeItem: (id) => {
+      cart.update((items) => {
+        return items.filter((item) => item.id !== id);
+      });
+    }
+  };
+}
+
+
+export default createCart();
